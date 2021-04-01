e025623122ce1c62f2c8fa8f0f2df3e93ad8f77a
unknown <Asia.rayaneh.65@gmail.com>
2021-04-02 00:17:07 +0430

    added dbcontext-unitofwork

---
+++ OnlineShop/OnlineShop.Persistence.EF/EFDataContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace OnlineShop.Persistence.EF
+{
+   public class EFDataContext:DbContext
+    {
+        public EFDataContext(DbContextOptions<EFDataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<AccountingDocument> AccountingDocuments { get; set; }
+        public DbSet<Good> Products { get; set; }
+        public DbSet<Category> ProductCategories { get; set; }
+        public DbSet<GoodEntry> ProductEntries { get; set; }
+        public DbSet<SalesInvoice> SalesInvoices { get; set; }
+        public DbSet<SalesItem> SalesItems { get; set; }
+        public DbSet<Warehouse> WarehouseItems { get; set; }
+    }
+}
---
+++ OnlineShop/OnlineShop.Persistence.EF/EFUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using OnlineShop.Infrastructure.Application;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Persistence.EF
+{
+    public class EFUnitOfWork : UnitOfWork
+    {
+        public void Complate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ComplateAysnc()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
